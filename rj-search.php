3cd924cb3d212d9169346dd75f590c25f38e6744
rony747 <touhid_rony@yahoo.com>
2022-11-23 11:41:38 +0600

    minor updates

--- rj-search.php
+++ rj-search.php
@@ -6,7 +6,7 @@
  * Version:           1.10.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
+ * Author:            t.i.rony
  * Author URI:        https://author.example.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
